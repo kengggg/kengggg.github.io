bdf2a5f67d40679fd8204e7a83ba4d80ac01dc3d
Keng Susumpow <patipat@me.com>
2026-01-05 10:52:47 +0700

    Update site URL to patipat.org and refine project description

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://patipat.blog',
+  site: 'https://patipat.org',
   integrations: [
     sitemap(),
   ],
